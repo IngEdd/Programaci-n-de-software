69a53ad04f2a528e082f650723c4363e6edfa65c
IngEdd <162384873+IngEdd@users.noreply.github.com>
2024-03-05 19:47:09 -0500

    Create Tabla2.java

---
+++ Tabla2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Tabla2 {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese un número para ver su tabla de multiplicar: ");
+        int numero = scanner.nextInt();
+
+        System.out.println("Tabla de multiplicar del " + numero + ":");
+
+        int i = 1;
+        while (i <= 10) {
+            int resultado = numero * i;
+            System.out.println(numero + " x " + i + " = " + resultado);
+            i++;
+        }
+    }
+}
+
+}

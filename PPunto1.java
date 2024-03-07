2fd621177cb074d617c7dd1d90fc33700b1cabf4
IngEdd <162384873+IngEdd@users.noreply.github.com>
2024-03-06 19:14:17 -0500

    ejercicios para repasar

---
+++ PPunto1.java
@@ -0,0 +1,21 @@
+/*
+
+1. Desarrolle un algoritmo que le permita leer dos valores y escribir la suma de los dos. 
+
+*/
+import java.util.Scanner;
+
+public class PPunto1 {
+    public static void main(String[]args){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("digite primer numero");
+    int num1 = sc.nextInt();
+    System.out.println("ingrese segundo numero: ");
+    int num2 =sc.nextInt();
+    int res = num1 + num2 ;
+    System.out.println("La suma de "+ num1 + " y " + num2 +" es: "+ res );
+
+    }
+        
+
+}
---
+++ PPunto2.java
@@ -0,0 +1,22 @@
+   /*
+   2.
+Desarrolle un algoritmo que le permita leer un valor entero, calcular su cuadrado y escribir
+dicho resultado.
+   
+   */ 
+import java.util.Scanner;
+
+public class PPunto2 {
+public static void main(String[] args) {
+
+    System.out.println("Calculadora de cuadrado, digite un numero para digitar su cuadrado: ");
+    Scanner sc = new Scanner(System.in);
+    int num1 = sc.nextInt();
+int res =num1 * num1;
+System.out.println("el cuadrado de "+ num1 + " es " + res);
+
+sc.close();
+
+}
+
+}
